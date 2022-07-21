fd4daae1f8dda65b4b57600d86b0a9b1ba18063b
JosueRoddy <96269163+RoddyAnchundia@users.noreply.github.com>
2022-07-21 00:13:25 -0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      'nav' : ['Montserrat', 'sans-serif'],
+      'image' : ['Overpass', 'sans-serif']
+    },
+    extend: {
+      backgroundImage: {
+        'body' : "url('/images/bg.jpeg')",
+        'secondImage1' : "url('/images/seconBox.jpeg')",
+        'secondImage2' : "url('/images/secondBox2.jpeg')",
+        'secondImage3' : "url('/images/secondBox3.jpeg')"
+      },
+      spacing: {
+        'slide': '550px',
+        'secondBox' : '400px',
+        'imagew' : '300px',
+        'footer' : '200px',
+        'newsW' : '380px',
+        'newsH' : '250px',
+        'medioW' : '450px',
+        'medioH' : '695px',
+      }
+    },
+  },
+  plugins: [],
+}
